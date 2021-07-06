e9f2cfce55cc41c388b00c55943cf3de98f21f5f
Brian Francis <13bfrancis@gmail.com>
2021-07-06 01:57:13 -0400

    added cypress for end to end testing of app

---
+++ cypress/integration/test.ts
@@ -0,0 +1,25 @@
+// test.ts created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe("testing", () => {
+  it("does something", () => {
+    cy.visit("https://google.com");
+    cy.title().should("match", /google/i);
+  });
+});
